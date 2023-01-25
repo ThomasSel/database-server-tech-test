f41654785890486d507bfdeacb110453c84ef785
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-25 17:18:30 +0000

    Add basic implementation of the brief
    This is just a placeholder to try out a quick solution with express
    
    The following is still needed:
    - Move routes to separate file
    - Move storage to separate file? -> needs to be compatible with
      adding a database
    - Use PUT instead of POST for /set???
    - TESTING

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello, world!");
+app.use(express.json());
+
+const memory = {};
+
+app.post("/set", (req, res) => {
+  Object.assign(memory, req.query);
+  res.status(200).json({ message: "OK" });
+});
+
+app.get("/get", (req, res) => {
+  value = memory[req.query.key];
+  if (value === undefined) {
+    res.status(400).send(`Key ${req.query.key} doesn't exist in memory`);
+  } else {
+    res.status(200).json({ value: value });
+  }
 });
 
 app.listen(port, () => {
