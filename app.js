cb8bfe41c8cca11d67251e3e18e6cacf79d48dd6
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-25 16:48:21 +0000

    Add nodemon to monitor changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello, world");
+  res.status(200).send("Hello, world!");
 });
 
 app.listen(port, () => {
