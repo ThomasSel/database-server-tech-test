914eb72afc36ce1b3dad8a3e275dc998fe14dc07
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-26 15:14:44 +0000

    Refactor set tests with beforeEach

--- spec/routes/set.test.js
+++ spec/routes/set.test.js
@@ -3,13 +3,16 @@ const app = require("../../app.js");
 
 describe("PUT /set", () => {
   describe("with one query parameter", () => {
-    it("responds with status 201", async () => {
+    let response;
+    beforeEach(async () => {
       response = await request(app).put("/set?name=John");
+    });
+
+    it("responds with status 201", () => {
       expect(response.status).toEqual(201);
     });
 
-    it("responds with a confirmation message", async () => {
-      response = await request(app).put("/set?name=John");
+    it("responds with a confirmation message", () => {
       expect(response.body.message).toEqual("OK");
     });
   });
