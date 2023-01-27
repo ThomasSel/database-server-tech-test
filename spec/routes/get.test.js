912bbb94d8ef11943ab374b528e20314c3f14d07
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-27 14:58:19 +0000

    Send 400 on GET /get without the "key" query param

--- spec/routes/get.test.js
+++ spec/routes/get.test.js
@@ -17,4 +17,18 @@ describe("GET /get", () => {
       expect(response.body.message).toEqual("Bad Request");
     });
   });
+
+  describe("without the key query parameter", () => {
+    beforeEach(async () => {
+      response = await request(app).get("/get?name=John&username=j0hn");
+    });
+
+    it("responds with status 400", async () => {
+      expect(response.status).toEqual(400);
+    });
+
+    it("responds with an error message", async () => {
+      expect(response.body.message).toEqual("Bad Request");
+    });
+  });
 });
